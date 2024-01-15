dcfb68b02508f99d7ce161b62288d0dc8f06baeb
Ignacio Mendez <ignacio.mendez.perez@gmail.com>
2024-01-15 11:00:41 -0300

    fix(iframe): fix existing iframe bug

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
 export const CONTAINER_ID = 'soyio-widget-iframe-container';
+export const IFRAME_ID = 'soyio-widget-iframe';
 export const DEVELOPMENT_WIDGET_URL = 'https://pl-soyio-staging-7a391ba45b99.herokuapp.com/widget';
--- src/widget.ts
+++ src/widget.ts
@@ -1,12 +1,17 @@
-import { CONTAINER_ID, DEVELOPMENT_WIDGET_URL } from './constants';
+import { CONTAINER_ID, IFRAME_ID, DEVELOPMENT_WIDGET_URL } from './constants';
 
 const WIDGET_URL = DEVELOPMENT_WIDGET_URL;
+
+function iframeExists(): boolean {
+  return !!document.getElementById(IFRAME_ID);
+}
 
 function getIframeContainer(): HTMLDivElement | undefined {
   return document.getElementById(CONTAINER_ID) as HTMLDivElement | undefined;
 }
 
-export function mountIframeToDOM(): HTMLIFrameElement {
+// eslint-disable-next-line max-statements
+function mountIframe(): HTMLIFrameElement {
   const iframeContainer = getIframeContainer();
   if (!iframeContainer) {
     throw new Error('Iframe container does not exist');
@@ -14,9 +19,19 @@ export function mountIframeToDOM(): HTMLIFrameElement {
 
   const iframe = document.createElement('iframe');
   iframe.src = WIDGET_URL;
+  iframe.id = IFRAME_ID;
+  iframe.style.zIndex = String(Number.MAX_SAFE_INTEGER);
   iframe.style.width = '100%';
   iframe.style.height = '100%';
   iframeContainer.appendChild(iframe);
 
   return iframe;
+}
+
+export function mountIframeToDOM(): HTMLIFrameElement {
+  if (!iframeExists()) {
+    return mountIframe();
+  }
+
+  return document.getElementById(IFRAME_ID) as HTMLIFrameElement;
 }
