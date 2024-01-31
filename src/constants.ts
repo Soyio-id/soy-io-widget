337f1fe85c954f356a06dbf95889b9bf9084789d
Ignacio Mendez <ignacio.mendez.perez@gmail.com>
2024-01-31 10:12:48 -0300

    fix(WidgetUrl): change widget url

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const CONTAINER_ID = 'soyio-widget-iframe-container';
 export const IFRAME_ID = 'soyio-widget-iframe';
-export const DEVELOPMENT_WIDGET_URL = 'https://pl-soyio-staging-7a391ba45b99.herokuapp.com/widget';
+export const DEVELOPMENT_WIDGET_URL = 'https://staging.soyio.id/widget';
