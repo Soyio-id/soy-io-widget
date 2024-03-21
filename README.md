8c10654188cfc503faffaba81828bd859b52a13d
Ignacio Mendez <ignacio.mendez.perez@gmail.com>
2024-03-21 19:36:48 -0300

    chore(new-version): support new flow

--- README.md
+++ README.md
@@ -10,23 +10,22 @@ For frontend frameworks, this should be imported as follows.
 
 ```html
 <script>
-  const flow = '<flow>' // Can only take the values of 'register' or 'authenticate'
-
-  const configProps = {
-    companyId: '<company id>',           // Starts with 'com_'
-    userReference?: '<user identifier of company>' // Optional
-    flowTemplateId: '<flow template id', // Starts with 'vft_' only needed in 'register' flow
-    identityId: '<identity id>'          // Starts with 'id_' only needed in 'authenticate' flow
-  }
-
-  const onEvent = (data) => {
-    console.log('APPLICATION: EVENT!');
-    console.log(data);
+  const widgetConfig = {
+    flow: '<flow>' // Can only take the values of 'register' or 'authenticate'
+    configProps = {
+      companyId: '<company id>',           // Starts with 'com_'
+      userReference: '<user identifier of company>' // Optional
+      userEmail: '<user email>'            // If not provided, Soyio will prompt the user to enter an email.
+      flowTemplateId: '<flow template id', // Starts with 'vft_' only needed in 'register' flow
+      identityId: '<identity id>'          // Starts with 'id_' only needed in 'authenticate' flow
+    }
+    onEvent: (data) => console.log('Data: ', data),
+    isSandbox: true // Optional. Default is false.
   }
 
   // Create widget when needed. In this example, widget is created when page is loaded.
   document.addEventListener('DOMContentLoaded', function() {
-    new Widget(flow, configProps, onEvent)
+    new Widget(widgetConfig)
   });
 </script>
 
--- src/widget.ts
+++ src/widget.ts
@@ -1,22 +1,5 @@
-import {
-  CONTAINER_ID,
-  IFRAME_ID,
-  BASE_URL,
-} from './constants';
-
-function getFullUrl(flow: Flow, configProps: ConfigProps, developmentUrl?: string): string {
-  let url = developmentUrl ?? BASE_URL;
-  url += `/${flow}?isWeb=true&companyId=${configProps.companyId}`;
-  if (flow === 'authenticate') {
-    url += `&identityId=${configProps.identityId}`;
-  } else if (flow === 'register') {
-    url += `&flowTemplateId=${configProps.flowTemplateId}`;
-  } else {
-    url += 'INVALID PARAMS: ';
-  }
-
-  return url;
-}
+import { CONTAINER_ID, IFRAME_ID } from './constants';
+import { getFullUrl } from './utils';
 
 function iframeExists(): boolean {
   return !!document.getElementById(IFRAME_ID);
@@ -26,27 +9,38 @@ function getIframeContainer(): HTMLDivElement | undefined {
   return document.getElementById(CONTAINER_ID) as HTMLDivElement | undefined;
 }
 
-// eslint-disable-next-line max-statements
-function mountIframe(flow: Flow, configProps: ConfigProps, developmentUrl?: string): HTMLIFrameElement {
+// eslint-disable-next-line max-statements, complexity
+function mountIframe(
+  flow: Flow,
+  configProps: Partial<ConfigProps>,
+  isSandbox: boolean,
+  developmentUrl: string | undefined,
+): HTMLIFrameElement {
   const iframeContainer = getIframeContainer();
   if (!iframeContainer) {
     throw new Error('Iframe container does not exist');
   }
 
   const iframe = document.createElement('iframe');
-  iframe.src = getFullUrl(flow, configProps, developmentUrl);
+  iframe.src = getFullUrl(flow, configProps, isSandbox, developmentUrl);
   iframe.id = IFRAME_ID;
   iframe.style.zIndex = String(Number.MAX_SAFE_INTEGER);
   iframe.style.width = '100%';
   iframe.style.height = '100%';
+  iframe.allow = 'autoplay; camera; publickey-credentials-get *; publickey-credentials-create *';
   iframeContainer.appendChild(iframe);
 
   return iframe;
 }
 
-export function mountIframeToDOM(flow: Flow, configProps: ConfigProps, developmentUrl?: string): HTMLIFrameElement {
+export function mountIframeToDOM(
+  flow: Flow,
+  configProps: Partial<ConfigProps>,
+  isSandbox: boolean,
+  developmentUrl: string | undefined,
+): HTMLIFrameElement {
   if (!iframeExists()) {
-    return mountIframe(flow, configProps, developmentUrl);
+    return mountIframe(flow, configProps, isSandbox, developmentUrl);
   }
 
   return document.getElementById(IFRAME_ID) as HTMLIFrameElement;
