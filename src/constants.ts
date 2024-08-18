c83ed7afaa67d1e3225ce66e97a1161d7ae0651b
ignaciomendezp <ignacio.mendez.perez@gmail.com>
2024-08-18 18:29:33 -0400

    feat(Constants): add new finishing events

--- src/constants.ts
+++ src/constants.ts
@@ -3,9 +3,10 @@ export const IFRAME_ID = 'soyio-widget-iframe';
 export const PRODUCTION_URL = 'https://app.soyio.id/widget';
 export const SANDBOX_URL = 'https://sandbox.soyio.id/widget';
 export const FINISHING_EVENTS = [
-  'IDENTITY_AUTHENTICATED',
+  'DISCLOSURE_REQUEST_SUCCESSFUL',
   'IDENTITY_REGISTERED',
   'IDENTITY_SIGNATURE',
+  'UNEXPECTED_ERROR',
   'DENIED_CAMERA_PERMISSION',
   'REJECTED_SIGNATURE',
 ];
