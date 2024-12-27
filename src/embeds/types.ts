2fcbbcdebbbb0755cd28629eb1961366c41bcf25
Ignacio Méndez <ignacio.mendez.perez@gmail.com>
2024-12-27 12:24:07 -0300

    feat(ConsentRequestBox): support height change events

--- src/embeds/types.ts
+++ src/embeds/types.ts
@@ -1,10 +1,18 @@
 export const IFRAME_EVENT = 'IFRAME_EVENT';
+export const IFRAME_HEIGHT_CHANGE = 'IFRAME_HEIGHT_CHANGE';
 
-export type ConsentRequestEvent = {
+export type ConsentCheckboxChangeEvent = {
   eventName: 'CONSENT_CHECKBOX_CHANGE'
   entityId: `ent_${string}`
   isSelected: boolean
 }
+
+export type IframeHeightChangeEvent = {
+  eventName: 'IFRAME_HEIGHT_CHANGE'
+  height: number
+}
+
+export type ConsentRequestEvent = ConsentCheckboxChangeEvent | IframeHeightChangeEvent;
 
 export type ConsentRequestConfig = {
   consentRequestId: `consreq_${string}`,
