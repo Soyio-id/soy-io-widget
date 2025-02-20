a599851e93ac673b928a7d029c95aaacb6d03b37
Ignacio Méndez <ignacio.mendez.perez@gmail.com>
2025-02-20 14:38:17 -0300

    feat(TooltipManager): export tooltip manager

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import { ConsentBox } from './embeds';
+import { TooltipManager } from './embeds/tooltip-manager';
 import * as SoyioTypes from './types';
 import { SoyioWidget } from './widget';
 
 export default SoyioWidget;
-export { ConsentBox, SoyioWidget, SoyioTypes };
+export {
+  ConsentBox,
+  SoyioWidget,
+  SoyioTypes,
+  TooltipManager as _TooltipManager,
+};
