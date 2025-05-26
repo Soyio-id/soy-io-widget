9ac74583c11395f8dc96494f13ae5c249109d4e5
Ignacio Mendez Perez <ignacio.mendez.perez@gmail.com>
2025-05-26 14:38:43 -0400

    feat(PrivacyCenter): support sessionToken param

--- src/embeds/privacy-center/types.ts
+++ src/embeds/privacy-center/types.ts
@@ -1,6 +1,17 @@
 import { BaseConfig } from '../base/types';
 
-export type PrivacyCenterConfig = BaseConfig & {
+export type PrivacyCenterConfigWithSessionToken = BaseConfig & {
+  sessionToken: string;
+  companyId?: never;
+  subjectId?: never;
+};
+
+export type PrivacyCenterConfigWithoutSessionToken = BaseConfig & {
+  sessionToken?: never;
   companyId: `com_${string}`;
   subjectId?: `ent_${string}`;
 };
+
+export type PrivacyCenterConfig =
+  | PrivacyCenterConfigWithSessionToken
+  | PrivacyCenterConfigWithoutSessionToken;
--- src/embeds/privacy-center/utils.ts
+++ src/embeds/privacy-center/utils.ts
@@ -3,21 +3,23 @@ import { PRIVACY_BASE_URL, PRIVACY_SANDBOX_URL } from '../../constants';
 import type { PrivacyCenterConfig } from './types';
 
 function getIframeUrl(privacyCenterConfig: PrivacyCenterConfig): string {
-  const URL_PARAMS = [
-    'subjectId',
-    'companyId',
-  ] as const;
-
   const isSandbox = privacyCenterConfig.isSandbox ?? false;
   const baseUrl = privacyCenterConfig.developmentUrl || (isSandbox ? PRIVACY_SANDBOX_URL : PRIVACY_BASE_URL);
 
   const urlParams = new URLSearchParams();
-  URL_PARAMS.forEach((param) => {
-    if (privacyCenterConfig[param]) urlParams.set(param, privacyCenterConfig[param]!);
-  });
+
+  if (privacyCenterConfig.sessionToken) {
+    urlParams.set('sessionToken', privacyCenterConfig.sessionToken);
+  } else if (privacyCenterConfig.companyId) {
+    urlParams.set('companyId', privacyCenterConfig.companyId);
+
+    if (privacyCenterConfig.subjectId) {
+      urlParams.set('subjectId', privacyCenterConfig.subjectId);
+    }
+  }
 
   const queryString = urlParams.toString();
-  return `${baseUrl}/${queryString ? `?${queryString}` : ''}`;
+  return `${baseUrl}${queryString ? `?${queryString}` : ''}`;
 }
 
 export { getIframeUrl };
